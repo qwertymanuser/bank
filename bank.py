956f800571ab7d9f915bbef7d0e16e68fa60efc7
qwertymanuser <marlenrai14@gmail.com>
2023-03-11 22:03:00 +0600

    second commit

--- bank.py
+++ bank.py
@@ -4,15 +4,17 @@ import sys
 import sqlite3
 import time
 
+
 class StartDB:
     def __init__(self):
         self.connect = sqlite3.connect('bank.db')
         self.connect.execute("""
             CREATE TABLE IF NOT EXISTS users(
-            login VARCHAR(255),
-            password VARCHAR(255),
-            email VARCHAR(255),
-            created VARCHAR(100)
+            login VARCHAR(255) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            created VARCHAR(100),
+            balance INT
             );
         """)
         self.connect.commit()
@@ -26,11 +28,14 @@ class SignUp(QWidget):
         self.db = StartDB()
         self.signup.clicked.connect(self.register)
 
+
     def hide_error(self):
         self.error.hide()
 
+
     def show_error(self):
         self.error.show()
+
 
     def register(self):
         login = self.login.text()
@@ -39,17 +44,74 @@ class SignUp(QWidget):
         self.show_error()
         cursor = self.db.connect.cursor()
         try:
-            cursor.execute(f"INSERT INTO users VALUES ('{login}', '{password}', '{email}', '{time.ctime()}')")
-            self.error.setText("Ваши данные успешно записаны")
-        except sqlite3.IntegrityError as d:
-            print(d.args)
-            if d.args == "('UNIQUE constraint failed: users.login',)":
-                self.error.setText("Данный логин занят")
-            elif d.args == ('UNIQUE constraint failed: users.email',):
-                self.error.setText("Данная почта занята")
+            cursor.execute(f"INSERT INTO users VALUES ('{login}', '{password}', '{email}', '{time.ctime()}', 0);")
+            self.error.setText("Успешно")
+        except sqlite3.IntegrityError as s:
+            print(s.args)
+            if s.args == "('UNIQUE constraint failed: users.login',)":
+                self.error.setText("Логин уже занят.")
+            elif s.args == ('UNIQUE constraint failed: users.email',):
+                self.error.setText("Почта уже занята.")
             else:
-                self.error.setText("Логин занят")
+                self.error.setText("Логин занят.")
         self.db.connect.commit()
+        
+
+class Personal(QWidget):
+    def __init__(self, login):
+        super(Personal, self).__init__()
+        self.login = login
+        loadUi('personal.ui', self)
+        self.username.setText(login)
+        self.db = StartDB()
+        cursor = self.db.connect.cursor()
+        result = cursor.execute(f"SELECT balance FROM users WHERE login = '{login}';")
+        self.balance.setText(f"{result.fetchall()[0][0]} KGS")
+        self.make.clicked.connect(self.make_money)
+        self.hide_transfer()
+        self.transfer.clicked.connect(self.user_transfer)
+        self.send.clicked.connect(self.user_transfer)
+
+
+    def hide_transfer(self):
+        self.login.hide()
+        self.amount.hide()
+        self.send.hide()
+        self.result.hide()
+
+    
+    def show_transfer(self):
+        self.login.show()
+        self.amount.show()
+        self.send.show()
+        self.result.show()
+
+
+    def update_balance(self):
+        cursor = self.db.connect.cursor()
+        result = cursor.execute(f"SELECT balance FROM users WHERE login = '{self.login}';")
+        self.balance.setText(f"{result.fetchall()[0][0]}")
+
+    
+    def make_money(self):
+        cursor = self.db.connect.cursor()
+        cursor.execute(f"UPDATE users SET balance = balance + {10} WHERE login = '{self.login}';")
+        self.db.connect.commit()
+        self.update_balance()
+
+
+    def user_transfer(self):
+        self.show_transfer()
+        login = self.login.text()
+        amount = self.amount.text()
+        cursor = self.sb.connect.cursor()
+        cursor.execute(f"SELECT login FROM users WHERE login = '{login}';")
+        result = cursor.fetchall()
+        if result != []:
+            cursor.execute(f"SELECT balance FROM users WHERE login = '{self.login}';")
+            user_balance = cursor.fetchall()[0][0]
+            if user_balance >= amount:
+                print("OK")
 
 
 class Bank(QMainWindow):
@@ -60,25 +122,36 @@ class Bank(QMainWindow):
         self.signin.clicked.connect(self.check_login)
         self.class_signup = SignUp()
         self.signup.clicked.connect(self.show_signup)
+        self.db = StartDB()
+
 
     def show_signup(self):
         self.class_signup.show()
 
+
     def hide_error(self):
         self.error.hide()
+
 
     def show_error(self):
         self.error.show()
 
+
     def check_login(self):
         login = self.login.text()
         password = self.password.text()
-        if login == 'geeks' and password == 'geeks2023':
+        cursor = self.db.connect.cursor()
+        cursor.execute(f"SELECT * FROM users WHERE login = '{login}' AND password = '{password}';")
+        result = cursor.fetchall()
+        if result != []:
             self.show_error()
             self.error.setText("Ok")
+            self.personal = Personal(login)
+            self.personal.show()
         else:
             self.show_error()
             self.error.setText("Неправильные данные")
+
 
 app = QApplication(sys.argv)
 bank = Bank()
